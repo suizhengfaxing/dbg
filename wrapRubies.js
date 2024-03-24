f75c1a6fab9fe6d4f251eb80c1d8dafbda8933c2
suizhengfaxing <86827626+suizhengfaxing@users.noreply.github.com>
2024-03-24 20:00:00 +0800

    Add files via upload

--- wrapRubies.js
+++ wrapRubies.js
@@ -2,15 +2,12 @@ function wrapRubies() {
     // 获取所有具有 class="container-content" 的 div 元素
     var containers = document.querySelectorAll('.container-content-ZhuYin');
 
-    // 循环遍历容器元素
-    for (var j = 0; j < containers.length; j++) {
-        var container = containers[j];
+    // 定义不应该出现在行首的标点符号集合
+    var punctuations = '!%),.:;>?]}¢¨°·ˇˉ―‖’”…‰′″›℃∶、。〃〉》」』】〕〗〞︶︺︾﹀﹄﹚﹜﹞！＂％＇），．：；？］｀｜｝～￠';
 
+    containers.forEach(function(container) {
         // 在当前容器内获取所有的 ruby 元素
         var rubies = container.querySelectorAll('ruby');
-
-        // 定义不应该出现在行首的标点符号集合
-        var punctuations = '!%),.:;>?]}¢¨°·ˇˉ―‖’”…‰′″›℃∶、。〃〉》」』】〕〗〞︶︺︾﹀﹄﹚﹜﹞！＂％＇），．：；？］｀｜｝～￠';
 
         for (var i = 1; i < rubies.length; i++) {
             var currentRuby = rubies[i];
@@ -22,26 +19,20 @@ function wrapRubies() {
                 var divWrapper = document.createElement('div');
                 divWrapper.style.display = 'inline-block';
 
-                // 克隆当前 ruby 元素和前一个 ruby 元素
-                var clonedPrevRuby = prevRuby.cloneNode(true);
-                var clonedCurrentRuby = currentRuby.cloneNode(true);
-
                 // 将新创建的 div 元素插入到当前 ruby 元素之前
                 container.insertBefore(divWrapper, currentRuby);
 
-                // 将克隆的 ruby 元素放入新创建的包裹容器中
-                divWrapper.appendChild(clonedPrevRuby);
-                divWrapper.appendChild(clonedCurrentRuby);
-
-                // 移除原始的 ruby 元素
-                container.removeChild(prevRuby);
-                container.removeChild(currentRuby);
+                // 将前一个 ruby 元素和当前 ruby 元素放入新创建的包裹容器中
+                var spaceNode = document.createTextNode(' ');
+                divWrapper.appendChild(prevRuby);
+                divWrapper.appendChild(spaceNode);
+                divWrapper.appendChild(currentRuby);
 
                 // 跳过当前的 ruby 元素，因为它已经与前一个一起处理了
                 i++;
             }
         }
-    }
+    });
 }
 
 wrapRubies();
